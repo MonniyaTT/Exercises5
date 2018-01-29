00cdfcba10a1fd3e6038c673b9243e5e3796a7fb
CITS_DELL_05\Tang <Tang30738@gmail.com>
2018-01-29 13:51:32 +0700

    first commit

--- Exercises5/Program.cs
+++ Exercises5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,39 @@ namespace Exercises5
     {
         static void Main(string[] args)
         {
+            StreamReader readwords = new StreamReader("../../words.txt");
+            string lineWords = readwords.ReadToEnd();
+            Console.WriteLine(lineWords);
+
+            Exercise1(lineWords);
+            Exercise2(lineWords);
         }
+
+        private static void Exercise2(string lineWords)
+        {
+            string wordsLong = "";
+            int count = 0;
+            var words = lineWords.Split(' ');
+
+            foreach (var item in words)
+            {
+                if (item.Length > count)
+                {
+                    count = item.Length;
+                    wordsLong = item;
+                }
+            }
+            Console.WriteLine("The longest word : " + wordsLong);
+
+        }
+
+        private static void Exercise1(string lineWords)
+        {
+            var words = lineWords.Split(' ');
+            int countWords = words.Length;
+            Console.WriteLine("The number of words : " + countWords);
+        }
+
+
     }
 }
